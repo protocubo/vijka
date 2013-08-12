a4bf92ab0594cda191755e8b52e0e632d6ea640e
Jonas Malaco Filho <jonas@jonasmalaco.com>
2013-08-12 19:03:25 -0300

    Minor reorganization

--- compile_sim.Makefile
+++ compile_sim.Makefile
@@ -1,19 +1,19 @@
 all: neko cpp64
 
 neko:
-	haxe compile_sim.neko.hxml
+	haxe ${HXFLAGS} compile_sim.neko.hxml
 run-neko: neko
 	exp/compile_sim/neko/CompileSim
 .PHONY: neko run-neko
 
 cpp64:
-	haxe compile_sim.cpp64.hxml --no-inline
+	haxe ${HXFLAGS} compile_sim.cpp64.hxml
 run-cpp64: cpp64
 	exp/compile_sim/cpp/local64/CompileSim
 .PHONY: cpp64 run-cpp64
 
 java:
-	haxe compile_sim.java.hxml
+	haxe ${HXFLAGS} compile_sim.java.hxml
 run-java: java
 	java -jar exp/compile_sim/java/java.jar
 .PHONY: java run-java
--- src/blah/CompileSim.hx
+++ src/blah/CompileSim.hx
@@ -8,6 +8,8 @@ import graph.linkList.Vertex;
 import sim.Simulator;
 
 class CompileSim {
+
+	@:access( graph.linkList.Digraph )
 	function new() {
 		var d = new Digraph();
 
@@ -29,10 +31,54 @@ class CompileSim {
 		d.simpleSSSPT( v1.node, 1.5, auto, new UserCostModel( 1.2, .3, 1 ), null );
 		trace( [ v1.dist, v1.time, v1.toll, v1.cost, v1.parent != null ? v1.parent.from.node.id : null ] );
 		trace( [ v2.dist, v2.time, v2.toll, v2.cost, v2.parent != null ? v2.parent.from.node.id : null ] );
+
+		#if cpp
+		cpp.vm.Profiler.start( "hxcpp_profiling" );
+		#end
+		trace( "******        TEST PATHS        ******" );
+		var v = 10;
+		var a = 20;
+		for ( n in 1...5 ) {
+			v *= 10;
+			a *= 10;
+			trace( '====== #v=$v, #a=$a ======' );
+			var d = new graph.linkList.Digraph();
+			trace( 'initialized a new digraph' );
+			var t0 = haxe.Timer.stamp();
+			for ( i in 0...v )
+				d.addVertex( new def.Node( i, i, i ) );
+			var tel = haxe.Timer.stamp() - t0;
+			trace( 'added $v vertices in $tel' );
+			var auto = new def.VehicleClass( 1, 1, 1, "Auto" );
+			var speed = new def.Speed();
+			speed.set( auto, 60 );
+			t0 = haxe.Timer.stamp();
+			for ( i in 0...a ) {
+				var s = d.vs.get( Std.random( v ) );
+				var t = s;
+				while ( s == t )
+					t = d.vs.get( Std.random( v ) );
+				d.addArc( new def.Link( i, s.node, t.node, Math.random()*10, speed, 0. ) );
+			}
+			tel = haxe.Timer.stamp() - t0;
+			trace( 'added $a arcs in $tel' );
+			t0 = haxe.Timer.stamp();
+			var ucost = new def.UserCostModel( 1., 0., 0. );
+			for ( i in 0...10 ) {
+				var s = d.vs.get( i );
+				d.simpleSSSPT( s.node, 0., auto, ucost );
+			}
+			tel = haxe.Timer.stamp() - t0;
+			trace( 'ran 10 single source shortest paths in $tel' );
+			trace( '... ${tel/10} per source, on average' );
+			trace( "" );
+		}
+		#if cpp
+		cpp.vm.Profiler.stop();
+		#end
 	}
 
 	static function main() {
 		var app = new CompileSim();
-		Simulator.main();
 	}
 }
